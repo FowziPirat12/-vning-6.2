083962943cf9f7a381aec47cbe203ffd61809b2e
melvinsjostrand <111962656+Dungeater1337@users.noreply.github.com>
2023-02-03 10:36:55 +0100

    Add project files.

---
+++ Övning 6.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Övning_6._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            SkrivBaklänges("Hejsan");
+            SkrivBaklänges("Detta blir Baklänges");
+            SkrivBaklänges("Även dätta går åt fel håll");
+        }
+
+        static void SkrivBaklänges(string meddelande)
+        {
+            for(int i= meddelande.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(meddelande[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
